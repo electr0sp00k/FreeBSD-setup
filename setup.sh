92f1359ed0195f338a78b4ff2845c389eac0629e
Teo <theodos@pdx.edu>
2023-06-14 15:48:27 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,38 +1,30 @@
 #!/bin/sh
 
-# Install FreeBSD, this part depends on your specific hardware and cannot be done through a script
-
 # Set your network interfaces names
 WAN="hn0"
 LAN="hn1"
 MIRROR="hn2"
 
 # Install dnsmasq
-pkg install dnsmasq -y
+pkg install dnsmasq
+
+# Enable forwarding
+echo "net.inet.ip.forwarding=1" >> /etc/sysctl.conf
+# Enable immediately
+sysctl net.inet.ip.forwarding=1
+
+# Set LAN IP
+ifconfig $LAN inet 192.168.1.1 netmask 255.255.255.0
+# Make IP setting persistent
+echo "ifconfig_$LAN=\"inet 192.168.1.1 netmask 255.255.255.0\"" >> /etc/rc.conf
 
 # Enable dnsmasq on boot
 sysrc dnsmasq_enable="YES"
-
-# Start dnsmasq
-service dnsmasq start
 
 # Edit dnsmasq configuration
 echo "interface=$LAN" >> /usr/local/etc/dnsmasq.conf
 echo "dhcp-range=192.168.1.50,192.168.1.150,12h" >> /usr/local/etc/dnsmasq.conf
 echo "dhcp-option=option:router,192.168.1.1" >> /usr/local/etc/dnsmasq.conf
-
-# Set LAN IP
-ifconfig $LAN inet 192.168.1.1 netmask 255.255.255.0
-
-# Make IP setting persistent
-echo "ifconfig_$LAN=\"inet 192.168.1.1 netmask 255.255.255.0\"" >> /etc/rc.conf
-
-# Enable forwarding
-echo "net.inet.ip.forwarding=1" >> /etc/sysctl.conf
-
-# Enable immediately
-sysctl net.inet.ip.forwarding=1
-
 # Configure PF for NAT
 echo "
 ext_if=\"$WAN\"
@@ -41,6 +33,9 @@ nat on \$ext_if from \$int_if:network to any -> (\$ext_if)
 pass in on \$int_if from \$int_if:network to any
 pass out on \$ext_if from any to any
 " >> /etc/pf.conf
+
+# Start dnsmasq
+service dnsmasq start
 
 # Enable PF on boot
 echo "pf_enable=\"YES\"" >> /etc/rc.conf
